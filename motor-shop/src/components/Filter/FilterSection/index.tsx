d6cc294d1c01ddf43e8be522328091bde56cb2cc
CToH10 <luis.ebn98@gmail.com>
2023-06-05 15:51:42 -0300

    feat: add basic selection behavior

--- motor-shop/src/components/Filter/FilterSection/index.tsx
+++ motor-shop/src/components/Filter/FilterSection/index.tsx
@@ -1,16 +1,36 @@
+import { useState } from "react";
+
 interface FilterSectionProps {
   title: string;
   values: Array<string | number>;
 }
 
 export const FilterSection = ({ title, values }: FilterSectionProps) => {
+  let translated: string;
+  const [render, setRender] = useState<Array<string | number>>(values);
+
   return (
-    <section className="filter-section">
-      <h2>{title}</h2>
-      {values.map((brandName: string | number) => {
+    <section className="filter-section mb-8">
+      <h2 className="text-2xl font-semibold text-black mb-7">{title}</h2>
+      {render.map((value: string | number) => {
+        switch (value) {
+          case "electric":
+            translated = "elétrico";
+            break;
+          case "hybrid":
+            translated = "híbrido";
+            break;
+        }
         return (
-          <h3 id={brandName.toString()} key={brandName}>
-            {brandName}
+          <h3
+            id={value.toString()}
+            key={value}
+            className="capitalize text-gray-500 font-medium"
+            onClick={() => {
+              setRender([value]);
+            }}
+          >
+            {translated ? translated : value}
           </h3>
         );
       })}
