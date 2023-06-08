1b50f64e6e12344c969cf954295da623e6809827
Gabriela <gabrielagarciafontoura@gmail.com>
2023-06-07 22:16:46 -0300

    fix: align image

--- motor-shop/src/pages/products/[productId].tsx
+++ motor-shop/src/pages/products/[productId].tsx
@@ -4,32 +4,28 @@ import { NewComment } from "@/components/commentNew";
 import ProductDetail from "@/components/productDetail";
 import { useRouter } from "next/router";
 
-export default function Products () {
-    const router = useRouter()
-    const { productId } = router.query
+export default function Products() {
+  const router = useRouter();
+  const { productId } = router.query;
 
-    return (
-        <>
-            <header>header</header>
-            <main  className="bg-grey-8">
-                
-                    <div className="bg-brand-1 h-96 absolute">
-                        <div className="lg:px-44">
-                            <section className="md:flex md:justify-between py-9 gap-5">
-                                <ProductDetail />
-                                <Aside />
-                            </section>
-                            <div>
-                                <CommentList/>
-                                <NewComment/>
-                            </div>
-                        </div>
-                    </div>
-             
-
-                
-            </main>
-            <footer>Same as homepage</footer>
-        </>
-    )
+  return (
+    <>
+      <header>header</header>
+      <main className="bg-grey-8 h-screen">
+        <div className="bg-brand-1 h-96 absolute">
+          <div className="lg:px-44">
+            <section className="md:flex md:justify-between py-9 gap-5">
+              <ProductDetail />
+              <Aside />
+            </section>
+            <div>
+              <CommentList />
+              <NewComment />
+            </div>
+          </div>
+        </div>
+      </main>
+      <footer>Same as homepage</footer>
+    </>
+  );
 }
