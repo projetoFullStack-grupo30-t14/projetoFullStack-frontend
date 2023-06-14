4f5a6e4da7a6b34ed8310d6505c25f28613b8e91
Lucas Wesley Fragoso <lucaswmmf@gmail.com>
2023-06-13 21:09:37 -0300

    feat-add list card seller

--- motor-shop/src/pages/index.tsx
+++ motor-shop/src/pages/index.tsx
@@ -2,6 +2,9 @@ import { Filter } from "@/components/Filter";
 import { Footer } from "@/components/headerAndFooter/footer";
 import { Header } from "@/components/headerAndFooter/header";
 import ListCards from "@/components/listCards";
+import CardCar from "@/components/cardCar";
+import { carsListMock } from "@/mocks/carList.mock";
+import { TCar } from "@/schemas/car.schema";
 
 export default function HomePage() {
   return (
@@ -9,15 +12,15 @@ export default function HomePage() {
       <Header />
       <div className="pb-11">
         <div className="relative mb-12 w-full flex justify-center items-center">
-            <figure className="bg-no-repeat bg-center"
-              style={{
-                backgroundImage: "url(/big_car_img_unfilter.png)",
-                height: "500px",
-                width: "100%"
-              }}
-            >
-              <div className="bg-gradient-to-b from-grey-black29 to-grey-black100 w-full h-full"></div>
-            </figure>
+          <figure className="bg-no-repeat bg-center"
+            style={{
+              backgroundImage: "url(/big_car_img_unfilter.png)",
+              height: "500px",
+              width: "100%"
+            }}
+          >
+            <div className="bg-gradient-to-b from-grey-black29 to-grey-black100 w-full h-full"></div>
+          </figure>
           <div className="absolute text-center">
             <p className="heading-3-500 mb-3 font-lexend sm:heading-1-700 text-grey-10">Motors Shop</p>
             <p className="heading-5-500 pb-80 px-6 font-lexend sm:heading-2-600 sm:pb-0 text-grey-10">A melhor plataforma de anúncios do país</p>
@@ -28,7 +31,11 @@ export default function HomePage() {
           className="flex flex-col gap-y-8 md:flex-row-reverse lg:flex-row-reverse md:gap-x-4 lg:gap-x-8"
         >
           <main>
-            <ListCards />
+            <ListCards carList={carsListMock}>
+              {(car: TCar) => (
+                <CardCar car={car} />
+              )}
+            </ListCards>
           </main>
           <aside className="relative z-10">
             <Filter />
@@ -36,7 +43,7 @@ export default function HomePage() {
         </section>
         {/* <footer>The page navigator</footer> */}
       </div>
-      <Footer/>
+      <Footer />
     </div>
   );
 }
--- motor-shop/src/pages/profile.tsx
+++ motor-shop/src/pages/profile.tsx
@@ -1,14 +1,25 @@
+import CardCarSeller from "@/components/cardCarSeller";
 import { Footer } from "@/components/headerAndFooter/footer";
 import { Header } from "@/components/headerAndFooter/header";
 import InfoSellerProfile from "@/components/infoSellerProfile";
+import ListCards from "@/components/listCards";
+import { carsListMock } from "@/mocks/carList.mock";
+import { TCar } from "@/schemas/car.schema";
 
 const ProfilePage = () => {
     return (
         <>
             <Header />
             <main className="pb-11 bg-gradient-to-b from-brand-1 from-30% to-grey-8 to-30%">
-                <div className="lg:px-44 py-9">
-                    <InfoSellerProfile/>
+                <div className="lg:px-44 pt-20">
+                    <InfoSellerProfile />
+                </div>
+                <div className="md:pl-20 sm:pl-4 py-6 w-full">
+                    <ListCards carList={carsListMock}>
+                        {(car: TCar) => (
+                            <CardCarSeller car={car} />
+                        )}
+                    </ListCards>
                 </div>
             </main>
             <Footer />
