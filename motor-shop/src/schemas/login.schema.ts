7639c4c0e4ecb75defd51b78c316eebb6ce06714
Gabriela <gabrielagarciafontoura@gmail.com>
2023-06-19 09:28:28 -0300

    fix: login schema

--- motor-shop/src/schemas/login.schema.ts
+++ motor-shop/src/schemas/login.schema.ts
@@ -1,7 +1,7 @@
 import { z } from "zod";
 
 export const loginSchema = z.object({
-  email: z.string().email().nonempty('Campo obrigatório'),
+  email: z.string().email("Email Inválido").nonempty('Campo obrigatório'),
   password: z.string().min(8, 'Senha deve ter no mínimo 8 caracteres').nonempty('Campo obrigatório'),
 });
 
