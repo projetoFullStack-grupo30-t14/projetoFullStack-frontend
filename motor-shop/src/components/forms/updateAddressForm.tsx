57f7e1fb32cc3de1659a1062e3cafbeb67341f48
CToH10 <luis.ebn98@gmail.com>
2023-06-27 14:15:22 -0300

    refactor: update address && working login

--- motor-shop/src/components/forms/updateAddressForm.tsx
+++ motor-shop/src/components/forms/updateAddressForm.tsx
@@ -4,15 +4,16 @@ import {
   tAddressResponse,
   tUserRegister,
   userRegisterSchema,
-} from '@/schemas/user.register.schema';
-import { useContext } from 'react';
-import { useForm } from 'react-hook-form';
-import { zodResolver } from '@hookform/resolvers/zod';
-import { UserInput } from '@/components/userInput';
-import { api } from '@/services';
-import { useModal } from '@/contexts/modalContext';
-import { UserContext } from '@/contexts/userContext';
-import { toast } from 'react-toastify';
+} from "@/schemas/user.register.schema";
+import { useContext } from "react";
+import { useForm } from "react-hook-form";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { UserInput } from "@/components/userInput";
+import { api } from "@/services";
+import { useModal } from "@/contexts/modalContext";
+import { UserContext } from "@/contexts/userContext";
+import { toast } from "react-toastify";
+import { Field } from "../Input";
 
 interface iAddressResponse {
   bairro: string;
@@ -30,8 +31,7 @@ interface iAddressResponse {
 
 export default function UpdateAddressForm() {
   const { closeModal } = useModal();
-  const { currAddress, setCurrAddress, headers } =
-    useContext(UserContext);
+  const { currAddress, setCurrAddress, headers } = useContext(UserContext);
 
   const {
     handleSubmit,
@@ -41,7 +41,7 @@ export default function UpdateAddressForm() {
     watch,
   } = useForm<tAddress>({
     resolver: zodResolver(addressSchema),
-    reValidateMode: 'onBlur',
+    reValidateMode: "onBlur",
     defaultValues: {
       cep: currAddress?.cep,
       city: currAddress?.city,
@@ -53,17 +53,17 @@ export default function UpdateAddressForm() {
   });
 
   const [wCep, wCity, wComplement, wNumber, wState, wStreet] = watch([
-    'cep',
-    'city',
-    'complement',
-    'number',
-    'state',
-    'street',
+    "cep",
+    "city",
+    "complement",
+    "number",
+    "state",
+    "street",
   ]);
 
   const onSubmit = async (data: tAddress) => {
     const address = (
-      await api.patch<tAddressResponse>('addresses', data, headers)
+      await api.patch<tAddressResponse>("addresses", data, headers)
     ).data;
     setCurrAddress(address);
     console.log(currAddress);
@@ -75,142 +75,117 @@ export default function UpdateAddressForm() {
       <main className="bg-grey-8">
         <div className="flex justify-center items-center h-full">
           <div className="z-10 h-full w-[410px] font-medium bg-grey-whiteFixed space-y-8 sm:min-w-max">
-            <form
-              className="flex flex-col"
-              onSubmit={handleSubmit(onSubmit)}
-            >
-              <p className="text-inputLabel mb-6">
-                Informações de endereço
-              </p>
-              <UserInput
+            <form className="flex flex-col" onSubmit={handleSubmit(onSubmit)}>
+              <p className="text-inputLabel mb-6">Informações de endereço</p>
+              <Field
                 label="CEP"
                 type="text"
                 placeholder="12345-678"
-                registerAddress={register}
-                db_field_address="cep"
+                register={register("cep")}
+                id="cep"
                 maxLength={9}
-                className={
-                  currAddress?.cep == wCep ? 'text-grey-3' : ''
-                }
+                className={currAddress?.cep == wCep ? "text-grey-3" : ""}
                 onChange={async (e) => {
-                  setValue('cep', e.target.value);
+                  setValue("cep", e.target.value);
                   if (e.currentTarget.value.length == 9) {
                     try {
-                      const cep = e.currentTarget.value
-                        .split('-')
-                        .join('');
-                      const response =
-                        await api.get<iAddressResponse>(
-                          `https://viacep.com.br/ws/${cep}/json/`
-                        );
+                      const cep = e.currentTarget.value.split("-").join("");
+                      const response = await api.get<iAddressResponse>(
+                        `https://viacep.com.br/ws/${cep}/json/`
+                      );
 
-                      setValue('state', response.data.uf);
+                      setValue("state", response.data.uf);
                       setValue(
-                        'street',
+                        "street",
                         `${response.data.logradouro}, ${response.data.bairro}`
                       );
-                      setValue('city', response.data.localidade);
+                      setValue("city", response.data.localidade);
 
                       if (response.data.erro) {
                         console.log(response);
-                        toast.error('CEP Inválido!');
-                        setValue('state', '');
-                        setValue('street', '');
-                        setValue('city', '');
+                        toast.error("CEP Inválido!");
+                        setValue("state", "");
+                        setValue("street", "");
+                        setValue("city", "");
                       }
                     } catch (error) {
                       console.log(error);
                     }
                   } else {
-                    setValue('state', '');
-                    setValue('street', '');
-                    setValue('city', '');
+                    setValue("state", "");
+                    setValue("street", "");
+                    setValue("city", "");
                   }
                 }}
               />
+
               <div className="flex w-fit gap-2 flex-1 box-border">
                 <div className="flex flex-col">
-                  <UserInput
+                  <Field
                     label="Estado"
                     type="text"
                     placeholder="Seu Estado"
-                    registerAddress={register}
-                    db_field_address="state"
+                    register={register("state")}
+                    id="state"
                     disabled={true}
                     className={
-                      currAddress?.state == wState
-                        ? 'text-grey-3'
-                        : ''
+                      currAddress?.state == wState ? "text-grey-3" : ""
                     }
                   />
                 </div>
                 <div className="flex flex-col">
-                  <UserInput
+                  <Field
                     label="Cidade"
                     type="text"
                     placeholder="Sua Cidade"
-                    registerAddress={register}
-                    db_field_address="city"
+                    register={register("city")}
+                    id="city"
                     disabled={true}
-                    className={
-                      currAddress?.city == wCity ? 'text-grey-3' : ''
-                    }
+                    className={currAddress?.city == wCity ? "text-grey-3" : ""}
                   />
                 </div>
               </div>
-              <UserInput
+              <Field
                 label="Endereço"
                 type="text"
                 placeholder="Logradouro e bairro"
-                registerAddress={register}
-                db_field_address="street"
+                register={register("street")}
+                id="street"
                 disabled={true}
-                className={
-                  currAddress?.street == wStreet ? 'text-grey-3' : ''
-                }
+                className={currAddress?.street == wStreet ? "text-grey-3" : ""}
               />
               {/* setValue( 'street', `${response.data.logradouro}, $
               {response.data.bairro}` ); */}
               <div className="flex w-fit gap-2 flex-1 box-border">
                 <div className="flex flex-col">
-                  <UserInput
+                  <Field
                     label="Número"
                     type="text"
                     placeholder="Ex: 22-A"
-                    registerAddress={register}
-                    db_field_address="number"
-                    onChange={(e) =>
-                      setValue('number', e.target.value)
-                    }
+                    register={register("number")}
+                    id="number"
+                    onChange={(e) => setValue("number", e.target.value)}
                     maxLength={5}
                     className={
-                      currAddress?.number == wNumber
-                        ? 'text-grey-3'
-                        : ''
+                      currAddress?.number == wNumber ? "text-grey-3" : ""
                     }
+                    error={errors.number?.message}
                   />
-                  {errors.number && (
-                    <small className="error">
-                      {errors.number.message}
-                    </small>
-                  )}
                 </div>
 
                 <div className="flex flex-col">
-                  <UserInput
+                  <Field
                     label="Complemento"
                     type="text"
                     placeholder="Ex: apart 307"
-                    registerAddress={register}
-                    db_field_address="complement"
-                    onChange={(e) =>
-                      setValue('complement', e.target.value)
-                    }
+                    register={register("complement")}
+                    id="complement"
+                    onChange={(e) => setValue("complement", e.target.value)}
                     maxLength={10}
                     className={
                       currAddress?.complement == wComplement
-                        ? 'text-grey-3'
-                        : ''
+                        ? "text-grey-3"
+                        : ""
                     }
                   />
                 </div>
@@ -236,4 +211,109 @@ export default function UpdateAddressForm() {
       </main>
     </>
   );
+}
+
+{
+  /* <UserInput
+label="CEP"
+type="text"
+placeholder="12345-678"
+registerAddress={register}
+db_field_address="cep"
+maxLength={9}
+className={currAddress?.cep == wCep ? "text-grey-3" : ""}
+onChange={async (e) => {
+  setValue("cep", e.target.value);
+  if (e.currentTarget.value.length == 9) {
+    try {
+      const cep = e.currentTarget.value.split("-").join("");
+      const response = await api.get<iAddressResponse>(
+        `https://viacep.com.br/ws/${cep}/json/`
+      );
+
+      setValue("state", response.data.uf);
+      setValue(
+        "street",
+        `${response.data.logradouro}, ${response.data.bairro}`
+      );
+      setValue("city", response.data.localidade);
+
+      if (response.data.erro) {
+        console.log(response);
+        toast.error("CEP Inválido!");
+        setValue("state", "");
+        setValue("street", "");
+        setValue("city", "");
+      }
+    } catch (error) {
+      console.log(error);
+    }
+  } else {
+    setValue("state", "");
+    setValue("street", "");
+    setValue("city", "");
+  }
+}}
+/> 
+<UserInput
+                    label="Complemento"
+                    type="text"
+                    placeholder="Ex: apart 307"
+                    registerAddress={register}
+                    db_field_address="complement"
+                    onChange={(e) => setValue("complement", e.target.value)}
+                    maxLength={10}
+                    className={
+                      currAddress?.complement == wComplement
+                        ? "text-grey-3"
+                        : ""
+                    }
+                  />
+                  <UserInput
+                    label="Número"
+                    type="text"
+                    placeholder="Ex: 22-A"
+                    registerAddress={register}
+                    db_field_address="number"
+                    onChange={(e) => setValue("number", e.target.value)}
+                    maxLength={5}
+                    className={
+                      currAddress?.number == wNumber ? "text-grey-3" : ""
+                    }
+                  />
+                  {errors.number && (
+                    <small className="error">{errors.number.message}</small>
+                  )}
+
+                   <UserInput
+                label="Endereço"
+                type="text"
+                placeholder="Logradouro e bairro"
+                registerAddress={register}
+                db_field_address="street"
+                disabled={true}
+                className={currAddress?.street == wStreet ? "text-grey-3" : ""}
+              />
+                  
+              <UserInput
+                    label="Cidade"
+                    type="text"
+                    placeholder="Sua Cidade"
+                    registerAddress={register}
+                    db_field_address="city"
+                    disabled={true}
+                    className={currAddress?.city == wCity ? "text-grey-3" : ""}
+                  />
+                  <UserInput
+                    label="Estado"
+                    type="text"
+                    placeholder="Seu Estado"
+                    registerAddress={register}
+                    db_field_address="state"
+                    disabled={true}
+                    className={
+                      currAddress?.state == wState ? "text-grey-3" : ""
+                    }
+                  />
+                  */
 }
--- motor-shop/src/pages/login.tsx
+++ motor-shop/src/pages/login.tsx
@@ -23,8 +23,7 @@ export default function LoginPage() {
   });
 
   const onSubmit = (data: TLogin) => {
-    // login(data);
-    console.log(data);
+    login(data);
   };
   return (
     <>
