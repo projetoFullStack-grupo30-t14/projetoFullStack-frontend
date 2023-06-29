c40c389a92e7bccb07311b8d722a69de7e05cf9f
CToH10 <luis.ebn98@gmail.com>
2023-06-29 15:02:16 -0300

    feat: add select model

--- motor-shop/src/components/forms/createAdForm.tsx
+++ motor-shop/src/components/forms/createAdForm.tsx
@@ -3,12 +3,12 @@ import { Field } from "../Input";
 import { useFieldArray, useForm } from "react-hook-form";
 import { zodResolver } from "@hookform/resolvers/zod";
 import { TCarRequest, carRequestSchema } from "@/schemas/car.schema";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { useModal } from "@/contexts/modalContext";
 
 export const CreateAdForm = () => {
   const { closeModal } = useModal();
-  const { createCar, listOneCar, carLoading } = useCars();
+  const { createCar, listOneCar, getBrandModels, models } = useCars();
 
   const {
     handleSubmit,
@@ -51,11 +51,9 @@ export const CreateAdForm = () => {
   ]);
 
   const onSubmit = (data: TCarRequest) => {
-    console.log(data);
     createCar(data);
-    // closeModal();
+    closeModal();
   };
-  console.log(errors);
 
   return (
     <main className="bg-grey-8">
@@ -71,20 +69,42 @@ export const CreateAdForm = () => {
               type="text"
               placeholder=""
               label="Marca"
-              onChange={(e) => setValue("brand", e.target.value)}
+              onChange={(e) => {
+                setValue("brand", e.target.value);
+                getBrandModels(e.target.value);
+              }}
               className={listOneCar?.brand === wBrand ? "text-grey-3" : ""}
               error={errors.brand?.message}
             />
-            <Field
-              id="model"
-              register={register("model")}
-              type="text"
-              placeholder=""
-              label="Modelo"
-              onChange={(e) => setValue("model", e.target.value)}
-              className={listOneCar?.model === wModel ? "text-grey-3" : ""}
-              error={errors.model?.message}
-            />
+            {models.length > 0 ? (
+              <div className="flex flex-col mb-8 max-h-[80px] overflow-y-visible">
+                <label htmlFor="model" className="text-inputLabel mb-3">
+                  Modelo
+                </label>
+                <select id="model" {...register("model")} className="scrollbar">
+                  <option value="" disabled selected>
+                    Selecione um modelo
+                  </option>
+                  {models.map((model) => (
+                    <option value={model} className="capitalize">
+                      {model}
+                    </option>
+                  ))}
+                </select>
+              </div>
+            ) : (
+              <Field
+                id="model"
+                register={register("model")}
+                type="text"
+                placeholder=""
+                label="Modelo"
+                onChange={(e) => setValue("model", e.target.value)}
+                defaultValue={listOneCar?.model}
+                className={listOneCar?.model === wModel ? "text-grey-3" : ""}
+                error={errors.model?.message}
+              />
+            )}
             <section className="grid grid-cols-2 justify-between gap-2 lg:min-w-max">
               <div className="flex flex-col">
                 <Field
