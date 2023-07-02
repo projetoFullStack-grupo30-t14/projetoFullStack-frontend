282568fb6a851f45a96ec8815fc54427289a8499
silvadpablo <teacherpablod@hotmail.com>
2023-07-02 20:38:13 -0300

    fixed next.config

--- motor-shop/next.config.js
+++ motor-shop/next.config.js
@@ -2,7 +2,8 @@
 const nextConfig = {
   reactStrictMode: true,
   env: {
-    api_base_url: "http://localhost:3001/",
+    // api_base_url: "http://localhost:3000/",
+    api_base_url: "https://motor-shop-grupo30.onrender.com/",
     timeout: 5000,
   },
 };
