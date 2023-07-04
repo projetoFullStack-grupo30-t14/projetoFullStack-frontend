b73b732d724419510d7adab445354f4f14c55a2e
Caio Alberto <caioalberto-@hotmail.com>
2023-07-04 15:01:10 -0300

    feat: adds conditional rendering of buy button & functionality

--- motor-shop/src/components/productDetail/productDetail.tsx
+++ motor-shop/src/components/productDetail/productDetail.tsx
@@ -1,7 +1,17 @@
-import React from "react";
-import { TCar } from "@/schemas/car.schema";
+import React, { useContext, useEffect, useState } from 'react';
+import { TCar } from '@/schemas/car.schema';
+import { UserContext } from '@/contexts/userContext';
+import { UserType } from '@/schemas';
+import { api } from '@/services';
+import { AxiosError } from 'axios';
+import { toast } from 'react-toastify';
+import { useRouter } from 'next/router';
+import Link from 'next/link';
 
 const ProductDetail = ({ car }: { car?: TCar }) => {
+  const { currUser } = useContext(UserContext);
+  const router = useRouter();
+
   return (
     <>
       <div className="flex flex-col gap-3">
@@ -19,12 +29,39 @@ const ProductDetail = ({ car }: { car?: TCar }) => {
           </h3>
           <div className="flex justify-between ">
             <div className="flex gap-2">
-              <span className="btn-brand-opacity btn-small"> {car?.year} </span>
-              <span className="btn-brand-opacity btn-small">{car?.mileage} KM</span>
+              <span className="btn-brand-opacity btn-small">
+                {' '}
+                {car?.year}{' '}
+              </span>
+              <span className="btn-brand-opacity btn-small">
+                {car?.mileage} KM
+              </span>
             </div>
-            <p className="heading-7-500 text-grey-1 ">R${car?.price.toLocaleString("pt-BR", { minimumFractionDigits: 2 })}</p>
+            <p className="heading-7-500 text-grey-1 ">
+              R$
+              {car?.price.toLocaleString('pt-BR', {
+                minimumFractionDigits: 2,
+              })}
+            </p>
           </div>
-          <button className="btn-brand1 btn-medium w-max">Comprar</button>
+
+          {currUser ? (
+            <Link
+              href={`https://wa.me/55${car?.user.phone}?text=Ol%C3%A1%2C+vi+seu+an%C3%BAncio+no+Motors+Shop+e+gostaria+de+conversar%21`}
+              target="_blank"
+            >
+              <button className="btn-brand1 btn-medium w-max">
+                Comprar
+              </button>
+            </Link>
+          ) : (
+            <button
+              className="btn-negative btn-medium w-max z-10"
+              onClick={() => router.push('/login')}
+            >
+              Comprar
+            </button>
+          )}
         </div>
 
         <div className="bg-grey-10 mt-4 rounded py-7 px-11">
